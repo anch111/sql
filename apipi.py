0c92f13b03e431fa5e639dbec738c308ccbdbac0
Anna_Zenkina <zenkina.21@yandex.ru>
2025-11-27 16:28:43 +0500

    Update apipi.py

--- apipi.py
+++ apipi.py
@@ -1,6 +1,6 @@
 # import requests
 # import json
-111
+11
 # def send_request(url):
 #     response = requests.get(url)
 #     if response.status_code == 200:
@@ -53,6 +53,7 @@ CREATE TABLE IF NOT EXISTS characters(
 ); 
 
 '''
+
 
 
 
