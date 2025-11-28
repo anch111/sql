e42ba0eb7bef22e33e4862ef178b3227dadcd522
Anna_Zenkina <zenkina.21@yandex.ru>
2025-11-28 22:32:47 +0500

    Update table.py

--- table.py
+++ table.py
@@ -1,7 +1,6 @@
 import sqlite3
 
 
-
 con = sqlite3.connect('db.sqlite')
 cur = con.cursor()
 
@@ -71,6 +70,7 @@ cur.executemany('INSERT INTO video_products2 VALUES(?, ?, ?);', video_products)
 
 con.commit()
 con.close()
+
 
 
 
